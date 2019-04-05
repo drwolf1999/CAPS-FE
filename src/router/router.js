e13b162222b3183379ed090307d16e7daee6fcde
root <drwolf1888@gmail.com>
2019-04-05 13:28:32 +0900

    test

--- src/router/router.js
+++ src/router/router.js
@@ -100,11 +100,11 @@ const router = new Router({
             name: '권한 필요!',
             component: NoAuth
         },
-        {
-            path: '/wiki',
-            name: '위키',
-            component: Wiki,
-        },
+        // {
+        //     path: '/wiki',
+        //     name: '위키',
+        //     component: Wiki,
+        // },
         {
             path: '/wiki/search/:wikiTitle',
             name: '위키',
