4a9905951653a4a23ed2da1ff6576fad557b7dfa
drwolf1999 <kdy19991222@gmail.com>
2019-04-11 03:30:59 +0000

    업데이트 'src/constants/RestAPI.js'
    RestApi.js fix

--- src/constants/RestAPI.js
+++ src/constants/RestAPI.js
@@ -1,6 +1,5 @@
 // 서버, API에 대한 상수들 정의
-// const SERVER_DOMAIN = 'http://caps.gq:8001/';
-const SERVER_DOMAIN = 'http://192.168.0.3:3000/';
+const SERVER_DOMAIN = 'http://caps.gq:8001/';
 const GET_PHOTO = SERVER_DOMAIN + 'gallery/photo/';
 
 export default {
